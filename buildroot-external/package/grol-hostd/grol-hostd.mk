e57b45e8f7be9ab6e46fa9f4b563221879099d8e
random coder <1538658+TheRetardedElon@users.noreply.github.com>
2026-09-25 18:41:14 -0700

    M2: create grol-hostd service user at image build time

--- buildroot-external/package/grol-hostd/grol-hostd.mk
+++ buildroot-external/package/grol-hostd/grol-hostd.mk
@@ -10,6 +10,13 @@ GROL_HOSTD_SITE_METHOD = local
 GROL_HOSTD_LICENSE = Apache-2.0
 GROL_HOSTD_GOMOD = github.com/grol5000/grol-hostd
 
+# Create the service identity in the target rootfs at build time. systemd must
+# be able to resolve User=/Group= before it can execute grol-hostd; sysusers.d
+# alone is too late/unreliable for this immutable HAOS-derived image path.
+define GROL_HOSTD_USERS
+	grol-hostd -1 grol-hostapi -1 * - /bin/false - GROL Host API
+endef
+
 define GROL_HOSTD_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(@D)/systemd/grol-hostd.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/grol-hostd.service
